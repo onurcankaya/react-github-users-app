237b6a91478d3ade5e0190bef29305507281a07a
Onur Cankaya <onur.eren.cankaya@gmail.com>
2022-07-05 15:24:11 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Dashboard } from './pages'
+
+export function App(): JSX.Element {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+export const Dashboard = (): JSX.Element => {
+  return (
+    <main>
+      <h2>Dashboard Page</h2>
+    </main>
+  )
+}
