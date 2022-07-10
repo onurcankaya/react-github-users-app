111215055264ee34589c99bfa03be7f9a73e7260
Onur Cankaya <onur.eren.cankaya@gmail.com>
2022-07-10 16:37:19 +0200

    feat: Button & LinkButton

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import styled, { css } from 'styled-components'
+
+const sharedStyles = css`
+  border-radius: var(--radius);
+  border-color: transparent;
+  padding: 0.5rem 1rem;
+  text-transform: capitalize;
+  background: var(--color-grey-5);
+  color: var(--color-white);
+  transition: var(--transition);
+  cursor: pointer;
+  font-size: 1rem;
+  &:hover {
+    background: var(--color-grey-6);
+  }
+`
+export const Button = styled.button`
+  ${sharedStyles}
+`
+export const LinkButton = styled.a`
+  ${sharedStyles}
+`
